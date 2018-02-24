d8acf2cdebee1533dab93f489fc32d48ddd4f0e2
mengyuan1996 <35983870+mengyuan1996@users.noreply.github.com>
2018-02-24 17:15:36 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package homework0124;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String stuNum;
+		String sex;
+		String Name;
+
+	}
+
+}
